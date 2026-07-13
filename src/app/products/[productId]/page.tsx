bcf43959484cb7806436a7a737d9b26c46a22d00
isollo <vovaisollo@gmail.com>
2026-07-12 22:30:12 -0700

    dynamic route

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+export default async function ProductDetails({params}: {
+    params: Promise<{ productId: string }>
+}) {
+    const productId = (await params).productId;
+
+    return (
+        <h1>Product {productId}</h1>
+    )
+}
