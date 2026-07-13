4f36316304ee5ec3b2dfd406882fa03ef106016a
isollo <vovaisollo@gmail.com>
2026-07-13 13:47:01 -0700

    destructure product id

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,7 @@
 export default async function ProductDetails({params}: {
     params: Promise<{ productId: string }>
 }) {
-    const productId = (await params).productId;
+    const {productId} = await params;
 
     return (
         <h1>Product {productId}</h1>
