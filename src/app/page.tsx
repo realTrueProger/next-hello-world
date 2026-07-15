0c5d00a65c413b13843d34ce5b19778120cabf64
isollo <vovaisollo@gmail.com>
2026-07-14 21:38:22 -0700

    add Links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <h1>Home page</h1>
+        <>
+            <h1>Home page</h1>
+            <Link href={"/about"}>About</Link>
+            <Link href={"/profile"}>Profile</Link>
+            <Link href={"/products"}>Products</Link>
+        </>
     )
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export default function ProductsList() {
+    const productLinks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
     return (
         <>
+            <Link href={"home"}>Home</Link>
             <h1>Product list</h1>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            {productLinks.map(link => (
+                <h2 key={link}><Link href={`products/${link}`}>Product {link}</Link></h2>
+            ))}
         </>
     )
 }
