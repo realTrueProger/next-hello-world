3520b80cb46a94b7765e811b5c38307a3ef2175d
isollo <vovaisollo@gmail.com>
2026-07-16 19:46:55 -0700

    loading example

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,6 @@
-export default function About() {
+export default async function About() {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
     return (
         <h1>About page</h1>
     )
