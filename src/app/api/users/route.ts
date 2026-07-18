6f12a2d9ba725d724c97f840dbc8b2c220584d97
isollo <vovaisollo@gmail.com>
2026-07-17 23:16:25 -0700

    add POST /users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,20 @@
-import {users} from "@/app/api/users/mock-users";
+import {User, users} from "@/app/api/users/mock-users";
+
+type CreateUserDto = Omit<User, "id">;
 
 export async function GET() {
     return Response.json(users)
+}
+
+export async function POST(request: Request) {
+    const user: CreateUserDto = await request.json();
+
+    const newUser: User = {
+        id: users.length + 1,
+        ...user
+    }
+
+    users.push(newUser);
+
+    return Response.json(newUser, { status: 201 });
 }
