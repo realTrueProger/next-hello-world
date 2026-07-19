38e0e177405c65a68edad6b2254244c904cdf217
isollo <vovaisollo@gmail.com>
2026-07-18 22:35:51 -0700

    add ?query params

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,16 @@
 import {User, users} from "@/app/api/users/mock-users";
+import {NextRequest} from "next/server";
 
 type CreateUserDto = Omit<User, "id">;
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const nameQuery = searchParams.get("name");
+
+    if (nameQuery) {
+        return Response.json(users.filter(user => user.name.includes(nameQuery)));
+    }
+
     return Response.json(users)
 }
 
@@ -16,5 +24,5 @@ export async function POST(request: Request) {
 
     users.push(newUser);
 
-    return Response.json(newUser, { status: 201 });
+    return Response.json(newUser, {status: 201});
 }
