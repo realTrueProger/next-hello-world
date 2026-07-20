c253a32440cf1b45049b346954de6f3d6b0fd5ba
isollo <vovaisollo@gmail.com>
2026-07-19 20:43:13 -0700

    add middleware example redirect

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    // return NextResponse.redirect(new URL("/", request.url));
+
+    if (request.nextUrl.pathname === "/profile") {
+        return NextResponse.redirect(new URL("/about", request.nextUrl));
+    }
+}
+
+// export const config = {
+//     matcher: "/profile",
+// };
